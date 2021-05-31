b75da1a8bc375c0104816566d8dad49de41be900
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-05-31 11:53:45 +0545

    initial commit

---
+++ LabOne/One.py
@@ -0,0 +1,19 @@
+'''
+1. Write a program that takes three numbers and prints their sum. Everynumber is given on a seperate line.
+'''
+
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: " ))
+num3 = int(input("enter the third value: "))
+sum = num1 + num2 + num3
+print(sum)
+print(f"the sum of three numbers is {sum}")
+
+'''
+2. Write a program that reads the length of the base and the height of a right-angled triangle and prints the area. Every number is given on a seperate line
+ '''
+
+num1 = int(input("enter the first value: "))
+num2 = int(input("enter the second value: "))
+num3 = int
+
