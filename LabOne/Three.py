6670817262bdcbfe7cd98af634e055cca12c96d4
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-05-31 12:40:09 +0545

    apples

--- LabOne/Three.py
+++ LabOne/Three.py
@@ -4,7 +4,7 @@
 
 stud = int(input("enter the number of students: "))
 appl = int(input("enter the number of apples: "))
-rem = stud - appl
 each = appl // stud
+rem = appl - (each * stud)
 print(f"the remaining numbers of apples is {rem}")
 print(f"each student gets {each} ")
