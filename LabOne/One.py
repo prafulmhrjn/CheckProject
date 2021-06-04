d49ca200a87f1e1c2692e4e086dd4f48c348e5de
Prafulmhrjn <praful.mhrjn29@gmail.com>
2021-06-04 11:16:12 +0545

    print statement change

--- LabOne/One.py
+++ LabOne/One.py
@@ -7,7 +7,7 @@ num2 = int(input("enter the second value: " ))
 num3 = int(input("enter the third value: "))
 sum = num1 + num2 + num3
 print(sum)
-print(f"the sum of three numbers is {sum}")
+print(f" sum of three numbers is {sum}")
 
 '''
 
